bb1a75641b1dbb7946bc8390b284982b9cb5da2c
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2022-03-21 21:16:29 -0600

    Started creating login function for event handling

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-from flask import Flask, render_template
+from wsgiref.validate import validator
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, FileField, TextField, DateField, validators
+from flask_login import LoginManager, UserMixin, login_user, current_user
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-class Room(db.Model):
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Float, nullable=False)
+    img_file = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String, unique=True, nullable=False)
-    commodities = db.Column(db.String, nullable=False)
-    size = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String, nullable=False)
 
 db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user_id
+
+class EventForm(Form):
+    name = TextField("Nombre", [validators.DataRequired()])
+    description = TextField("Descripción", [validators.DataRequired()])
+    date = TextField("Fecha", [validators.DataRequired()])
+    image = FileField("Imagen", validators=[validators.DataRequired()])
 
 @app.route("/") 
 def home():
@@ -28,7 +51,7 @@ def restaurant():
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html")
+    return render_template("blog.html", is_authenticated=current_user.is_authenticated)
 
 @app.route("/reservation")
 def reservation():
@@ -41,6 +64,15 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        pass
+        #hash password
+    
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1")
